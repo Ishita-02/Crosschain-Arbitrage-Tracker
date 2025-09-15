3c07250182c003b62dc9072d36e494d9a610f12d
Ishita Agarwal <ishitagrawal0207@gmail.com>
2025-09-15 17:04:48 +0530

    updated backend url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001'; 
+const API_BASE_URL = "https://crosschain-arbitrage-tracker.onrender.com"; 
 
 const LiveIcon = () => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="mr-2">
